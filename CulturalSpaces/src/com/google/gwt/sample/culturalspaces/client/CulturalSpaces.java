dbd1ccf2845469adad0b6aba80811c34e2b8cccf
Ian Wong <baller_id_7@hotmail.com>
2014-11-04 14:04:06 -0800

    added map function w/ login

--- CulturalSpaces/src/com/google/gwt/sample/culturalspaces/client/CulturalSpaces.java
+++ CulturalSpaces/src/com/google/gwt/sample/culturalspaces/client/CulturalSpaces.java
@@ -12,6 +12,17 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Anchor;
 
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.maps.client.InfoWindowContent;
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.Maps;
+import com.google.gwt.maps.client.control.LargeMapControl;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.maps.client.overlay.Marker;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
 public class CulturalSpaces implements EntryPoint {
 
 private VerticalPanel mainPanel = new VerticalPanel(); 
@@ -59,6 +70,7 @@ private void loadLogin() {
 private void loadCulturalSpaces() {
 	// Set up sign out hyperlink.
     signOutLink.setHref(loginInfo.getLogoutUrl());
+   
 	
 	// Create table for stock data.  
 	locationsFlexTable.setText(0, 0, "Location Name"); 
@@ -70,11 +82,49 @@ private void loadCulturalSpaces() {
     mainPanel.add(addPanel);
     mainPanel.add(lastUpdatedLabel);
     
+    /*
+     * Asynchronously loads the Maps API.
+     *
+     * The first parameter should be a valid Maps API Key to deploy this
+     * application on a public server, but a blank key will work for an
+     * application served from localhost.
+    */
+    Maps.loadMapsApi("AIzaSyCDJ525-htk7oLSiDCX3FopEs1UBY2K4JA", "2", false, new Runnable() {
+       public void run() {
+         buildUi();
+       }
+     });
+    
+    
 	// TODO Associate the Main panel with the HTML host page.
     RootPanel.get("locationList").add(mainPanel);
 
-
 }
+
+private void buildUi() {
+	// Using RootLayoutPanel to create 	a dynamic application
+    // Open a map centered on Vancouver, BC
+    LatLng vancouverBC = LatLng.newInstance(49.2496600, -123.1193400);
+
+    final MapWidget map = new MapWidget(vancouverBC, 11);
+    map.setSize("100%", "100%");
+    // Add some controls for the zoom level
+    map.addControl(new LargeMapControl());
+
+    // Add a marker
+    map.addOverlay(new Marker(vancouverBC));
+
+    // Add an info window to highlight a point of interest
+    map.getInfoWindow().open(map.getCenter(),
+        new InfoWindowContent("Vancouver, BC"));
+
+    final DockLayoutPanel dock = new DockLayoutPanel(Unit.PX);
+    dock.addNorth(map, 500);
+
+    // Add the map to the HTML host page
+    RootLayoutPanel.get().add(dock);
+  }
+
 
 private void handleError(Throwable error) {
     Window.alert(error.getMessage());
