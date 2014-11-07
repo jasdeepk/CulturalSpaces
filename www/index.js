c9555b8edcd10996401fadc9cbdb9fd6db304cad
Thomas Lamb <z3f6@ugrad.cs.ubc.ca>
2014-11-06 22:04:10 -0800

    Table displaying but not being filled. locations.php returns contents of table.

--- www/index.js
+++ www/index.js
@@ -1,5 +1,25 @@
-require(["jquery"], function ($) {
-	$.ready(function () {
-		
-	})
-})
+require(["jquery"], (function ($) {
+	console.log("required");
+	function fillTable (rows) {
+		var $table = $("#table");
+		for (var i = 0; i <= rows.length-1; i++)
+		{
+			var row = rows[i];
+			var $row = 
+				$("<tr>")
+					.append("<td>"+row['lat']+"</td>")
+					.append("<td>"+row['lon']+"</td>")
+					.append("<td>"+row['name']+"</td>")
+					.append("<td>"+row['addr']+"</td>")
+			;
+			$table.append(row);
+		}
+	}
+	
+	$(document).ready(function () {
+		console.log("ready");
+		debugger;
+		var locationRequest = $.getJSON("locations.php");
+		locationRequest.done(function (data) {fillTable(data);});
+	});
+}));
