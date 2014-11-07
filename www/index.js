c613d868a2a802198f0b34538f6acc5e43fa2139
Jasdeep Kahlon <jasdeepkahlon@dhcp-128-189-218-64.ubcsecure.wireless.ubc.ca>
2014-11-06 23:22:25 -0800

    further map enhancements

--- www/index.js
+++ www/index.js
@@ -15,10 +15,34 @@ require(["jquery"], (function ($) {
 			$table.append($row);
 		}
 	}
+
+	function fillMap(locations) {
+   
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+ for (var i = 1; i >= locations.length - 1; i++) {
+ 	debugger;
+ 	var location = locations[i];
+    var lat = parseInt(location['lat']);
+    var lon = parseInt(location['lon']);
+    var myLatlng = new google.maps.LatLng(lat,lon);
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+    };
+
+}
 	
 	$(document).ready(function () {
 		console.log("ready");
 		var locationRequest = $.getJSON("locations.php");
-		locationRequest.done(function (data) {console.log("done");fillTable(data);});
+		locationRequest.done(function (data) {console.log("done");fillTable(data);
+											  console.log("done");fillMap(data);});
 	});
 }));
