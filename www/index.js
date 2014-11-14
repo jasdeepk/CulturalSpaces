d8870b11f0c943ff1e6f51ff32373a2e5ca32577
Thomas Lamb <z3f6@ugrad.cs.ubc.ca>
2014-11-14 15:36:37 -0800

    Map shows correct coordinates.

--- www/index.js
+++ www/index.js
@@ -27,8 +27,8 @@ require(["jquery", "datatables"], (function ($) {
 
  for (var i = 0; i <= locations.length - 1; i++) {
  	var location = locations[i];
-    var lat = parseInt(location['lat']);
-    var lon = parseInt(location['lon']);
+    var lat = parseFloat(location['lat']);
+    var lon = parseFloat(location['lon']);
     var myLatlng = new google.maps.LatLng(lat,lon);
     var marker = new google.maps.Marker({
       position: myLatlng,
