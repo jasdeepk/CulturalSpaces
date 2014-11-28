cec4306cc6a60e24aa2e50d1315c3d8db813be4e
Thomas Lamb <z3f6@ugrad.cs.ubc.ca>
2014-11-28 12:04:45 -0800

    Favouriting works, unfavouriting does not.

--- www/favourites.php
+++ www/favourites.php
@@ -2,21 +2,43 @@
 error_reporting (-1);
 ini_set('display_errors', -1);
 ini_set("display_errors", 1);
-echo 'hi';
-if (isset($_POST['variable'])) {
-      echo $_POST['variable'];
-      $userID = $_POST['variable'];
-    } 
 
-try {
-    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=culturalspaces', "root", "root");
-    $statement = $dbh->query('SELECT locationID from favourites where userID='$userID'');
-    $favourites = $statement->fetchAll(PDO::FETCH_ASSOC);
-    print json_encode($favourites);
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+$dbh = new PDO('mysql:host=localhost;port=3306;dbname=culturalspaces', "root", "root");
+if (isset($_GET['userID'])) {
+    $userID = $_GET['userID'];
+
+    try {
+        $statement = $dbh->query('SELECT locationID from favourites where userID='.$userID);
+        $favourites = $statement->fetchAll(PDO::FETCH_ASSOC);
+        print json_encode($favourites);
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+else if (isset($_POST['isFavourited'])) {
+    $conn = new PDO("mysql:host=localhost;dbname=culturalspaces","root","root");
+    $isFavourited = $_POST['isFavourited'];
+    $locationID = $_POST['locationID'];
+    $userID = $_POST['userID'];
+
+    error_log("isFav = $isFavourited");
+    error_log("locationID = $locationID");
+    error_log("userID = $userID");
+
+    
+    if ($isFavourited == "true")
+    {
+        $sql = "INSERT INTO favourites (userID,locationID) VALUES (:userID,:locationID)";
+        $q = $conn->prepare($sql);
+        $q->execute(array(':userID'=>$userID, ':locationID'=>$locationID));    
+    }
+    else
+    {
+        $sql = "DELETE from favourites where userID = ".$userID." AND locationID = ".$locationID;
+        $q = $conn->query($sql);
+    }
 }
 
 ?>
--- www/index.js
+++ www/index.js
@@ -1,105 +1,120 @@
-require(["jquery", "datatables"], (function ($) {
-	console.log("required");
-	function fillTable (rows) {
-		var $table = $("#table");
-		for (var i = 0; i <= rows.length-1; i++)
-		{
-			var row = rows[i];
-			// turn the sites in the web row into actual links
-			if (row['web'][0] == "h" && row['web'][1] == "t" && row['web'][2] == "t" && row['web'][3] == "p") {
-				var result = row['web'].link(row['web']);
+require(["jquery", "datatables"], function ($) {
+	setTimeout(loadUserDependentData, 1000)
+	function loadUserDependentData ()
+	{
+		gapi.client.plus.people.get({'userId':'me'}).then(function(res) {
+			//shorthand
+			var userID = res.result.id;
+			console.log("User ID: "+userID);
+			
+			function fillTable (rows, favourites) {
+				var $table = $("#table");
+				for (var i = 0; i <= rows.length-1; i++)
+				{
+					var row = rows[i];
+					
+					//check to see if the current row describes a favourite location
+					var isFavourite = false;
+					var locationID = row['name'];
+					for (var j = 0; isFavourite === false && j <= favourites.length-1; j++)
+					{
+						var favouriteID = favourites[j]['locationID'];
+						if (locationID === favouriteID) isFavourite = true;
+					}
+
+					// turn the sites in the web row into actual links
+					if (row['web'][0] == "h" && row['web'][1] == "t" && row['web'][2] == "t" && row['web'][3] == "p") {
+						var result = row['web'].link(row['web']);
+					}
+					if (row['web'] == "") {
+						var result = "";
+					}
+					else result = row['web'].link("http://"+row['web']);
+					var $row = 
+						$("<tr data-locationid='"+row['name']+"'>")
+							.append("<td class='star"+(isFavourite? " favorited":"")+"' id='"
+								+row['name']
+								+"'></td>")
+							.append("<td>"+row['lat']+"</td>")
+							.append("<td>"+row['lon']+"</td>")
+							.append("<td>"+row['name']+"</td>")
+							.append("<td>"+row['addr']+"</td>")
+							.append("<td>"+result+"</td>")
+							.append("<td>"+row['type']+"</td>")
+							.append("<td>"+row['hood']+"</td>")
+					;
+					$table.append($row);
+				}
 			}
-			if (row['web'] == "") {
-				var result = "";
+
+			function fillMap(locations) {
+		   
+				var mapOptions = {
+			    	center: { lat: 49.2500, lng: -123.1000},
+			    	zoom: 12
+			    };
+				var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+				for (var i = 0; i <= locations.length - 1; i++) {
+				 	var location = locations[i];
+				    var lat = parseFloat(location['lat']);
+				    var lon = parseFloat(location['lon']);
+				    var myLatlng = new google.maps.LatLng(lat,lon);
+				    var marker = new google.maps.Marker({
+				      position: myLatlng,
+				      map: map,
+				      title: location.name
+				  	});
+			    };
 			}
-			else result = row['web'].link("http://"+row['web']);
-			var $row = 
-				$("<tr>")
-					.append("<td class='star' id='"
-						+row['name']
-						+"'></td>")
-					.append("<td>"+row['lat']+"</td>")
-					.append("<td>"+row['lon']+"</td>")
-					.append("<td>"+row['name']+"</td>")
-					.append("<td>"+row['addr']+"</td>")
-					.append("<td>"+result+"</td>")
-					.append("<td>"+row['type']+"</td>")
-					.append("<td>"+row['hood']+"</td>")
-			;
-			$table.append($row);
-		}
+
+			$(document).ready(function () {
+				var locationsRequest = $.getJSON("locations.php");
+				var favouritesRequest = $.getJSON("favourites.php", {userID: userID});
+				locationsRequest.then(function (locations) {
+					favouritesRequest.then(function (favourites) {
+						fillTable(locations, favourites);
+
+						fillMap(locations);
+
+						$("#table").DataTable({
+				            "sPaginationType": "full_numbers",
+				             "iDisplayLength": 4,
+				             "columns": [
+						    null,
+						    null,
+						    null,
+						    null,
+						    { "width": "10%" },
+						    null,
+						    null
+						  ],
+						     "aoColumns": [
+		  					{ "bSortable": false },
+		  					null,
+		  					null,
+		  					null,
+		  					null,
+		 					null,
+		  					null,
+		  					null,
+		  					]
+		        		});
+					})
+				});
+			    $('html').on('click', '.star', function() {
+			    	console.log('clicked');
+			        var id = $(this).attr('id');
+			        $(this).toggleClass('favorited');
+			        var postData = {'isFavourited': $(this).hasClass('favorited').toString(), 'userID': userID.toString(), 'locationID': $(this).parent().data('locationid').toString()};
+			        $.post('/favourites.php'
+			        	, postData
+			        	, function(data) { 
+			            	//some sort of update function here
+			        	}
+			        );
+			    });
+			});
+		});
 	}
-
-	function fillMap(locations) {
-   
-  var mapOptions = {
-          center: { lat: 49.2500, lng: -123.1000},
-          zoom: 12
-        };
- var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-
- for (var i = 0; i <= locations.length - 1; i++) {
- 	var location = locations[i];
-    var lat = parseFloat(location['lat']);
-    var lon = parseFloat(location['lon']);
-    var myLatlng = new google.maps.LatLng(lat,lon);
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: location.name
-  });
-    };
-
-}
-
-
-
-	
-	$(document).ready(function () {
-		console.log("ready");
-		var locationRequest = $.getJSON("locations.php");
-		locationRequest.done(
-			function (data) {
-				console.log("done");
-				fillTable(data);
-
-				fillMap(data);
-
-				$("#table").DataTable({
-		            "sPaginationType": "full_numbers",
-		             "iDisplayLength": 4,
-		             "columns": [
-				    null,
-				    null,
-				    null,
-				    null,
-				    { "width": "10%" },
-				    null,
-				    null
-				  ],
-				     "aoColumns": [
-  					{ "bSortable": false },
-  					null,
-  					null,
-  					null,
-  					null,
- 					null,
-  					null,
-  					null,
-  					]
-        		} );
-			}
-		);
-    $('html').on('click', '.star', function() {
-    	console.log('clicked');
-        var id = $(this).attr('id');
-        $(this).toggleClass('favorited');
-        $.post('/favourites.php', 
-               {'isFavorited': $(this).hasClass('favorited'), 'id': id},
-                  function(data) { 
-                     //some sort of update function here
-                  });
-        });
-     });
-}));
+});
