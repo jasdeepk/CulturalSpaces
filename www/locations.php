1f79bef1520d4da90ecd7e527e28b5181e583a27
Jasdeep Kahlon <jasdeep.kln@gmail.com>
2014-11-28 07:48:53 -0800

    locations now parses the remote file, puts it into the database, and pulls it into the tables

--- www/locations.php
+++ www/locations.php
@@ -22,7 +22,8 @@ try {
         )
     );
     */
-    $pdo = new PDO('mysql:host=mysql.culturalspaces.lambyte.com;port=3306;dbname=culturalspaces', "lambyte", "p0000000", array(PDO::MYSQL_ATTR_LOCAL_INFILE=>1));
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=culturalspaces', "root", "root", array(PDO::MYSQL_ATTR_LOCAL_INFILE=>1));
+
 } catch (PDOException $e) {
     die("database connection failed: ".$e->getMessage());
 }
@@ -30,15 +31,35 @@ try {
 //clear table
 $pdo->exec("TRUNCATE TABLE locations");
 
+
+$row = 1;
+
+$output = 'outbound.csv';
+$oh = fopen($output, 'w');
+
+if (($handle = fopen("inbound.csv", "r")) !== FALSE) {
+    fgetcsv($handle, 1000, ",");
+
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        
+        //$outputData = join(",", array($data[11], $data[10], $data[0], $data[4], $data[1], $data[2], $data[5]))."\n";
+        fputcsv($oh, array($data[11], $data[10], $data[0], $data[4], $data[1], $data[2], $data[5]));
+    }
+    fclose($handle);
+    fclose($oh);
+} 
+
+
 //parse file's data into table
 $affectedRows = $pdo->exec("
-    LOAD DATA LOCAL INFILE ".$pdo->quote("inbound.csv")." INTO TABLE `$databasetable`
+    LOAD DATA LOCAL INFILE ".$pdo->quote("outbound.csv")." INTO TABLE `$databasetable`
       
       FIELDS OPTIONALLY ENCLOSED BY '\"' TERMINATED BY ".$pdo->quote($fieldseparator)." 
       LINES TERMINATED BY ".$pdo->quote($lineseparator));
 
 try {
-    $dbh = new PDO('mysql:host=mysql.culturalspaces.lambyte.com;port=3306;dbname=culturalspaces', "lambyte", "p0000000");
+    $dbh = new PDO('mysql:host=localhost;port=3306;dbname=culturalspaces', "root", "root");
+    //$dbh = new PDO('mysql:host=mysql.culturalspaces.lambyte.com;port=3306;dbname=culturalspaces', "lambyte", "p0000000");
     $statement = $dbh->query('SELECT * from locations');
     $locations = $statement->fetchAll(PDO::FETCH_ASSOC);
     print json_encode($locations);
